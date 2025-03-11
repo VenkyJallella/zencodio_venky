ecc6df10305a1369c6ccea12a2f56802b5843b8c
Venky Jallella <jallellavenky42@gmail.com>
2025-03-11 19:03:13 +0530

    debug changed

--- zencodio_project/settings.py
+++ zencodio_project/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'IlkQQLfenkZaiWb5fnFNxytowzs7GdW5D9Bp_vSA-XtwuQwarZLmqXcvk2kdwUMqHqk')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS =  os.environ.get('ALLOWED_HOSTS', '').split(',')
 
