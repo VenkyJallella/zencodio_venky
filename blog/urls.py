a88e6f7f92e0f0ab5d0f416289ee76f69404e733
Venky Jallella <jallellavenky42@gmail.com>
2025-03-11 21:13:55 +0530

    updated urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from .views import upload_image
 from django.urls import path
 from .views import home, blog_detail
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -9,3 +11,6 @@ urlpatterns = [
     path('<slug:slug>/',blog_detail,name='blog_detail'),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
