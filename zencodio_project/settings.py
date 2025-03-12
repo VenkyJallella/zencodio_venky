49b13b0aaa47293304d90ced65377408603e10c2
Venky Jallella <jallellavenky42@gmail.com>
2025-03-12 07:40:14 +0530

    added database

--- zencodio_project/settings.py
+++ zencodio_project/settings.py
@@ -87,10 +87,7 @@ WSGI_APPLICATION = 'zencodio_project.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Database file path
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
