e9ace17edf058727af543e95195147e0264b2caa
Venky Jallella <jallellavenky42@gmail.com>
2025-03-12 20:24:50 +0530

    updated blog view upload_image

--- blog/views.py
+++ blog/views.py
@@ -4,15 +4,21 @@ from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 from django.shortcuts import render , get_object_or_404
 from .models import blogpost
+import cloudinary
 
 
 
 @csrf_exempt
 def upload_image(request):
     if request.method == 'POST' and request.FILES:
-        image = request.FILES['file']  # TinyMCE sends file as 'file'
-        path = default_storage.save('uploads/' + image.name, ContentFile(image.read()))
-        image_url = '/media/' + path  # Media URL for serving images
+        image = request.FILES['file']  # TinyMCE sends the file as 'file'
+
+        # Upload the image to Cloudinary
+        result = cloudinary.uploader.upload(image)
+
+        # Get the secure URL of the uploaded image
+        image_url = result.get("secure_url")
+
         return JsonResponse({'location': image_url})  # TinyMCE expects 'location'
     
     return JsonResponse({'error': 'Invalid request'}, status=400)
